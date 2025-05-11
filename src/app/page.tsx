20253e7ffec7f9d8cc956d4e2f7875fbe687dc94
César Amorim <cesar.amorim49@gmail.com>
2025-05-11 16:43:02 -0300

    chore: remove unnecessary default files from Next.js
    Removed boilerplate files and folders generated by create-next-app that won't be used in the project.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function HomePage() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <h1 className='text-6xl font-bold text-blue-500 hover:text-blue-50 hover:bg-blue-500 transition duration-1000'>
+        Texto no meu H1
+      </h1>
+    </div>
+  );
 }
