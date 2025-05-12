138167b247af640bb2a7e4ebe38875e324e710e5
César Amorim <cesar.amorim49@gmail.com>
2025-05-11 22:29:57 -0300

    feat: create post model structure

---
+++ src/models/post/post-model.ts
@@ -0,0 +1,12 @@
+export type PostModelo = {
+  id: string;
+  title: string;
+  slug: string;
+  excert: string;
+  content: string;
+  coverImageUrl: string;
+  publish: boolean;
+  createdAt: string;
+  updatedAt: string;
+  author: string;
+};
